5709d69cbdff56525d09f53a1bbc950f557e2eeb
scarecrow173 <akiyoshi.koyama@gmail.com>
2020-07-31 02:10:48 +0900

    update cmake build script&exec authority

--- build_vst_linux.sh
+++ build_vst_linux.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+PluginSrcPath="`pwd`/Orange"
+
 if [ ! -e build]; then mkdir build;fi
 pushd build
 if [ ! -e linux]; then mkdir linux;fi 
 pushd linux
 
-"../../tools/cmake/linux/x86_64/bin/cmake" -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=`dirname $0` "../../dependencies/vstsdk"
+"../../tools/cmake/linux/x86_64/bin/cmake" -DCMAKE_BUILD_TYPE=Debug -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH="`dirname $0`/Orange" "../../dependencies/vstsdk"
 "../../tools/cmake/linux/x86_64/bin/cmake" --build . --config Debug
+
+"../../tools/cmake/linux/x86_64/bin/cmake" -DCMAKE_BUILD_TYPE=Release -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH="`dirname $0`/Orange" "../../dependencies/vstsdk"
 "../../tools/cmake/linux/x86_64/bin/cmake" --build . --config Release
 
 popd
--- build_vst_mac.sh
+++ build_vst_mac.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+PluginSrcPath="`pwd`/Orange"
+
 if [ ! -e build ]; then mkdir build;fi
 pushd build
 if [ ! -e mac ]; then mkdir mac;fi
 pushd mac
 
-"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=`dirname $0` "../../dependencies/vstsdk"
+"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Debug -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath "../../dependencies/vstsdk"
 "../../tools/cmake/mac/CMake.app/Contents/bin/cmake" --build . --config Debug
+
+"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Release -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath "../../dependencies/vstsdk"
 "../../tools/cmake/mac/CMake.app/Contents/bin/cmake" --build . --config Release
 
 popd
