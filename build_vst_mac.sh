a4e4ebecaa72f35759c57d76fb79cc8d0629dd09
scarecrow173 <akiyoshi.koyama@gmail.com>
2020-08-01 23:13:57 +0900

    xcode project を生成するように変更

--- build_vst_mac.sh
+++ build_vst_mac.sh
@@ -7,10 +7,10 @@ pushd build
 if [ ! -e mac ]; then mkdir mac;fi
 pushd mac
 
-"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Debug -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath "../../dependencies/vstsdk"
+"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Debug -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath -GXcode "../../dependencies/vstsdk"
 "../../tools/cmake/mac/CMake.app/Contents/bin/cmake" --build . --config Debug
 
-"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Release -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath "../../dependencies/vstsdk"
+"../../tools/cmake/mac/CMake.app/Contents/bin/cmake" -DCMAKE_BUILD_TYPE=Release -DSMTG_ADD_VST3_HOSTING_SAMPLES=FALSE -DSMTG_ADD_VST3_PLUGINS_SAMPLES=FALSE DVSTGUI_TOOLS=FALSE -DSMTG_MYPLUGINS_SRC_PATH=$PluginSrcPath -GXcode "../../dependencies/vstsdk"
 "../../tools/cmake/mac/CMake.app/Contents/bin/cmake" --build . --config Release
 
 popd
