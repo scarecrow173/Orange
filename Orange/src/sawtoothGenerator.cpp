7952a1d2dbb63ed6594c7dabe171581facaf110c
shinta <akamatsu.dtm@gmail.com>
2020-08-09 01:42:14 +0900

    sawtooth試作

--- Orange/src/sawtoothGenerator.cpp
+++ Orange/src/sawtoothGenerator.cpp
@@ -1,4 +1,7 @@
-﻿#include "sawtoothGenerator.h"
+#include "sawtoothGenerator.h"
+#include <cmath>
+#include <limits>
+
 using namespace Orange;
 using namespace Orange::Oscillator;
 
@@ -20,7 +23,22 @@ void SawtoothGenerator::generate(int channels, int samples)
 	// Buffer.buffer[1]にsamples数のRチャンネルサンプルデータを入れる
 	// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 	//==================================================
-
+    const double deltatime = 1/sampleFrequency;
+    const double fullPeriodTime = 1/frequency;
+    
+    if (time >= std::numeric_limits<float>::max()) {
+            time = 0.0;
+    }
+    
+    for(int i = 0; i < samples; ++i){
+        const double localtime = fmod(time,fullPeriodTime);
+        const float value = amplitude * ((localtime / fullPeriodTime) * 2 - 1.0);
+        
+        Buffer.buffer[0][i] = value;
+        Buffer.buffer[1][i] = value;
+        
+        time += deltatime;
+    }
 	//==================================================
 	// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 	//==================================================
